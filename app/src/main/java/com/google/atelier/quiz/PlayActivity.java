b72322b1748fcb0109d7316e1a812e1c574d3bee
luigibolovan <luigibolovan@gmail.com>
2020-02-06 21:40:50 +0200

    Finished layouts

--- app/src/main/java/com/google/atelier/quiz/PlayActivity.java
+++ app/src/main/java/com/google/atelier/quiz/PlayActivity.java
@@ -2,7 +2,10 @@ package com.google.atelier.quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class PlayActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class PlayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+
+        ImageView playButton = (ImageView)findViewById(R.id.i_play);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent starter = new Intent(PlayActivity.this, QuizActivity.class);
+                startActivity(starter);
+            }
+        });
     }
 }
